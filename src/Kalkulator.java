ed743297089f269b7b0408c1aa6b8fd2e667dc21
Adrian <a.razniak.9@gmail.com>
2022-11-24 18:22:30 +0100

    UAT

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Kalkulator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Witaj w kalulatorze\n podaj pierwszą licze");
